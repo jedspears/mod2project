713f251c2b49ead990cd72cd45caace91232f687
Qunli <qunli02@gmail.com>
2019-05-29 10:28:17 -0400

    lock out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,45 @@
 class ApplicationController < ActionController::Base
+
+  before_action :authorized #lock down this whole app
+  helper_method :current_user #i can call current_user from a view
+
+skip_before_action :authorized, only: [:confirm, :login, :root]
+
+
+def current_user
+  # User.find would throw an error if we cannot find the user
+  User.find_by({ id: session[:user]})
+end
+
+def logged_in?
+  # nil is falsey in ruby
+  # a user instance is truthy
+  !!current_user
+end
+
+def authorized
+  redirect_to login_path unless logged_in?
+end
+
+
+
+
   def root
     @user = User.new
   end
 
   def home
-    @user = User.new(session[:user])
+    @user = User.find_by(id: session[:user])
   end
 
   def confirm
     @user = User.find_by(username: params[:username])
 
     if @user && @user.authenticate(params[:password])
-    session[:user] = User.find_by(username: params[:username])
-    @user = session[:user]
+    session[:user] = @user.id
     redirect_to home_path
     else
-    flash[:notice] = "Incorrect Username or password"
+    flash[:notice] = "Invalid Username or password"
     redirect_to login_path
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:new, :create]
+
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-
     if @user.save(user_params)
-      session[:user] = @user
+      session[:user] = @user[:id]
       redirect_to home_path
     else
       flash[:notice] = @user.errors.full_messages[0]
