25d0b14be7a9b868f488e313c7f2cd1617eb2adc
jedspears <jed.spears7@gmail.com>
2019-05-30 16:02:49 -0400

    finishedlowkey

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -12,20 +12,40 @@ class BetsController < ApplicationController
   end
 
   def index
+    # byebug
     @user = current_user
+    @active_bets = @user.bets.select { |bet| bet.status == "active" }
+    @closed_bets = @user.bets.select { |bet| bet.status == "closed" }
     # byebug
   end
 
   def show
-    byebug
+    # byebug
     @bet = Bet.find(params[:id])
   end
+
+  def orange
+    @bet = Bet.find(params[:id])
+  end
+
+  def update
+    @bet = Bet.find(params[:id])
+    if @bet.update(bet_params)
+      redirect_to bets_path
+    end
+    @bet.assign_winner
+  end
+
+  # def delete
+  #   Bet.find(params[:id]).destroy
+  #   redirect_to bets_path
+  # end
 
 
 
   private
 
   def bet_params
-    params.require(:bet).permit(:description, :username1, :username2, :amount, :accept?)
+    params.require(:bet).permit(:description, :username1, :username2, :amount, :accept, :winner1, :winner2, :real_winner)
   end
 end
